cf1973b125b13466f5bc3d7bf252ed082679df03
bprlab <bprlab@10.201.64.224>
2019-04-27 12:49:16 +0300

    ders 9 spring boot örnek

---
+++ Ders09Ornek/src/main/java/tr/edu/medipol/yazilimarac/Ders09Ornek/Ders09OrnekApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazilimarac.Ders09Ornek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders09OrnekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders09OrnekApplication.class, args);
+	}
+
+}
---
+++ Ders09Ornek/src/test/java/tr/edu/medipol/yazilimarac/Ders09Ornek/Ders09OrnekApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.edu.medipol.yazilimarac.Ders09Ornek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ders09OrnekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
